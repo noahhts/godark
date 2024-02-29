021fdef2c6af0f7af55e1cb1b238ae82a1b4a551
Noah Sorensen <100672306+noahhts@users.noreply.github.com>
2024-02-29 12:26:03 -0500

    create dark-mode cli for macos

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	numArgs := len(os.Args) - 1
+	if numArgs < 1 || numArgs > 1 {
+		fmt.Println("dark takes one argument: on, off, or toggle")
+	} else {
+		darkStatus := os.Args[1]
+		if darkStatus != "on" && darkStatus != "off" && darkStatus != "toggle" {
+			fmt.Println("dark takes one argument: on, off, or toggle")
+		} else {
+			var cmd *exec.Cmd
+			if darkStatus == "on" {
+				cmd = exec.Command("osascript", "-e", `tell application "System Events" to tell appearance preferences to set dark mode to true`)
+			} else if darkStatus == "off" {
+				cmd = exec.Command("osascript", "-e", `tell application "System Events" to tell appearance preferences to set dark mode to false`)
+			} else {
+				cmd = exec.Command("osascript", "-e", `tell application "System Events" to tell appearance preferences to set dark mode to not dark mode`)
+			}
+			err := cmd.Run()
+			fmt.Println("dark mode: " + darkStatus)
+			log.Printf("Command finished with error: %v", err)
+		}
+	}
+}
