914bee094546b30d835fd5ac49fb5c7226664951
Noah Sorensen <100672306+noahhts@users.noreply.github.com>
2024-02-29 23:46:00 -0500

    add help docs

--- main.go
+++ main.go
@@ -9,13 +9,23 @@ import (
 
 func main() {
 	numArgs := len(os.Args) - 1
-	if numArgs == 0 {
-		fmt.Println(`dark tells your system's appearance preferences to set dark mode to:
-			on			dark mode on
-			off			dark mode off
-			toggle	toggle dark mode`)
+	if numArgs == 0 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println(`dark is a CLI tool for dark mode
+It tells your system's appearance preferences to set dark mode with one command.
+
+Usage:
+  dark [command]
+
+Available Commands:
+  on	     Turn dark mode on
+  off	     Turn dark mode off
+  toggle     Toggle dark mode
+			
+Flags:
+  -h, --help	help for dark`)
+		return
 	}
-	if numArgs < 1 || numArgs > 1 {
+	if numArgs > 1 || numArgs < 1 {
 		fmt.Println("dark takes one argument: on, off, or toggle")
 	} else {
 		darkStatus := os.Args[1]
